a92546fadb8c4ae1fbfa4397146d10c8be76997f
aishik-pyne <aishikpyne@gmail.com>
2019-06-08 16:34:20 +0000

    Download Script

---
+++ data/download_places365_x256.sh
@@ -0,0 +1,9 @@
+wget http://data.csail.mit.edu/places/places365/train_256_places365challenge.tar
+wget http://data.csail.mit.edu/places/places365/val_256.tar
+wget http://data.csail.mit.edu/places/places365/test_256.tar
+wget http://data.csail.mit.edu/places/places365/filelist_places365-standard.tar
+
+tar -xvf train_256_places365challenge.tar
+tar -xvf val_256.tar
+tar -xvf test_256.tar
+tar -xvf filelist_places365-standard.tar
